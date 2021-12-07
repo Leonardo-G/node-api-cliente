4e43fadd59365d2f02e4329e4bb3dcd198e7f9ac
leonardog9823 <guanuco43@gmail.com>
2021-12-07 15:00:46 -0300

    verbo post disponible

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,5 +1,40 @@
-const usuariosPost = (req, res) => {
-    console.log(req.body);
+const Usuario = require("../models/Usuario");
+const bcrypt = require("bcryptjs");
+const { validationResult } = require("express-validator");
+
+const usuariosPost = async (req, res) => {
+
+    //Revisamos si hay errores
+    const errores = validationResult(req);
+    if( !errores.isEmpty() ){
+        return res.status(400).json({ errores: errores.array() })
+    }
+
+    const { nombre, email, password } = req.body;
+
+    const isExistEmail = await Usuario.findOne({ email });
+    
+    if(isExistEmail){
+        return res.status(400).json({msg: "Ya existe el usuario"});
+    }
+    
+    try {
+        //Hashear el password
+        const hash = await bcrypt.genSalt(10);
+        const hashPassword = await bcrypt.hash(password, hash);
+
+        //Crea el nuevo usuario
+        const usuario = new Usuario({ nombre, email, password: hashPassword });
+
+        // //guarda el usuario
+        await usuario.save();
+
+        res.status(201).json(usuario);
+
+    } catch (error) {
+        console.log(error);
+        res.status(400).json({msg: "Error al realizar POST"})
+    }
 }
 
 module.exports = {
--- route/usuarios.js
+++ route/usuarios.js
@@ -2,8 +2,13 @@
 const app = require("express");
 const { usuariosPost } = require("../controllers/usuarios");
 const router = app.Router();
+const { check } = require("express-validator");
 
 //Crea un usuario
-router.post( "/", usuariosPost );
+router.post( "/", [
+    check("nombre", "El nombre es obligatorio").not().isEmpty(),
+    check("email", "Agrega un email válido").isEmail(),
+    check("password", "El password debe ser minimo 6 caracteres").isLength({min: 6})
+], usuariosPost );
 
 module.exports = router;
