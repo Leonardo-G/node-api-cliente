41189a8e783788a10d3c4cca501729d4cd26bcb1
leonardog9823 <guanuco43@gmail.com>
2023-04-06 03:20:13 -0300

    cors in nest

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -7,6 +7,7 @@ export class AppController {
 
   @Get()
   getHello(): string {
+    console.log(process.env.SECRETA);
     return this.appService.getHello();
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger'
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('API administrador')
+    .setDescription('API para administrador de tareas')
+    .setVersion('2.0')
+    .addTag('cats')
+    .build();
+  
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup("docs", app, document)
+
+  app.enableCors();
   await app.listen(3000);
 }
 bootstrap();
