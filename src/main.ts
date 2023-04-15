3343cd45291ce77fe2bb518177e5d5a955780eba
leonardog9823 <guanuco43@gmail.com>
2023-04-15 04:12:37 -0300

    enable cors

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,13 @@ import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
+  
   app.useGlobalPipes( new ValidationPipe({
     whitelist: true,
     transform: true,
@@ -22,7 +29,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup("docs", app, document)
 
-  app.enableCors();
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
