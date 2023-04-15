a5068ab7396f8d1e6532449b690bae65ac5aeeb1
leonardog9823 <guanuco43@gmail.com>
2023-04-15 04:21:59 -0300

    modified cors

--- src/main.ts
+++ src/main.ts
@@ -6,11 +6,12 @@ import { ValidationPipe } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: '*',
-    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization'],
-    credentials: true,
-  });
+  origin: '*',
+  methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+  preflightContinue: false,
+  optionsSuccessStatus: 204,
+  credentials: true,
+});
   
   app.useGlobalPipes( new ValidationPipe({
     whitelist: true,
